bdfd2a4346292736e8ac6fa97674e43755d8f3a2
Nicholas Finch <nicfinch223@gmail.com>
2020-02-02 00:50:23 -0500

    Still need figure out a local storage

--- script.js
+++ script.js
@@ -2,21 +2,22 @@ $(document).ready(function() {
     var dt = new Date();
     document.getElementById('date').innerHTML = dt.toLocaleDateString();
 
-    var scheduleTimes = [
-        '9am: ',
-        '10am: ',
-        '11am: ',
-        '12pm: ',
-        '1pm: ',
-        '2pm: ',
-        '3pm: ',
-        '4pm: ',
-        '5pm: ',
-    ];
 
-    for(var i = 0; i < scheduleTimes.length; i++) {
-        var newDiv = $('<div>' + scheduleTimes[i] + '</div>');
-        $('#schedule').append(newDiv);
-        $('#schedule').addClass('.divPlan');
-    };
+    //event handlers for the save buttons
+    //grab user text and add to local storage
+    $('.saveBtn').on('click', function() {
+        event.preventDefault();
+        var input = ($(this).prev().children('.form-control').val());
+        localStorage.setItem(input);
+    });
+
+    //set item
+
+
+
+
+
+
+
+    
 });
