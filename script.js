5788d991f9a62b803f867d3919bd5444bc098f95
Nicholas Finch <nicfinch223@gmail.com>
2020-02-05 17:05:03 -0500

    finished local storage should add more css

--- script.js
+++ script.js
@@ -2,18 +2,37 @@ $(document).ready(function() {
     var dt = new Date();
     document.getElementById('date').innerHTML = dt.toLocaleDateString();
 
+    refresh();
 
-    //event handlers for the save buttons
-    //grab user text and add to local storage
+
     $('.saveBtn').on('click', function() {
         event.preventDefault();
         var input = ($(this).prev().children('.form-control').val());
-        localStorage.setItem(input);
+        var id = ($(this).prev().children('.form-control').attr('id'));
+        localStorage.setItem('input' + id, input);
+
     });
 
-    //set item
 
 
+
+    function refresh() {
+        var currentTime = dt.getHours();
+        for(var i = 9; i < 18; i++) {
+            var times = 'input' + i;
+            $('#'+i).val(localStorage.getItem(times));
+            if(currentTime === i) {
+                $('#'+i).css('background-color', 'green');
+                $('#'+i).css('color', 'white');
+            } else if(currentTime < i) {
+                $('#'+i).css('background-color', 'red');
+                $('#'+i).css('color', 'white');
+            } else {
+                $('#'+i).css('background-color', 'blue');
+                $('#'+i).css('color', 'white');
+            }
+        }
+    }
 
 
 
